61fcd07c03205788c15be351a4849a50bec874e4
Peter <peterpark29c1@gmail.com>
2020-02-18 12:26:29 -0600

    Updates

---
+++ precache-manifest.7e2ae61c4cf2dfc8d633c2c8bc755657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a331f1edc753bb0c9206e627cdbd31a1",
+    "url": "/robotos-rolodex/index.html"
+  },
+  {
+    "revision": "9046f5b34174cc5448be",
+    "url": "/robotos-rolodex/static/css/main.8a83626f.chunk.css"
+  },
+  {
+    "revision": "fcdd128eef2034e2c8ea",
+    "url": "/robotos-rolodex/static/js/2.4fc5497d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robotos-rolodex/static/js/2.4fc5497d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9046f5b34174cc5448be",
+    "url": "/robotos-rolodex/static/js/main.61aa7f9c.chunk.js"
+  },
+  {
+    "revision": "fb4da185fdc570adb445",
+    "url": "/robotos-rolodex/static/js/runtime-main.d1277a9d.js"
+  }
+]);
